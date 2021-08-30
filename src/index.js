7119b311180a020afbb7638d79fcad66d9b8022c
fraterpavlo <shukan1999@mail.ru>
2021-08-30 21:28:42 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let stack = [];
+    let map = new Map(bracketsConfig);
+
+    for (i=0; i<str.length; ++i) {
+        let currentBracket = str[i]; 
+        let lastBracketInStack = stack[stack.length - 1];
+        let pairForLastOpenBracket = map.get(lastBracketInStack);
+        if (map.has(currentBracket)) {
+          if (currentBracket == pairForLastOpenBracket) {
+            stack.pop();
+          } else {
+            stack.push(currentBracket);
+          }
+        } else {
+          if (stack.length == 0) {
+              return false;
+          } else {
+            if (currentBracket == pairForLastOpenBracket) {
+              stack.pop();
+            } else {
+              return false;
+            }
+          }
+      }
+  }
+  return stack.length === 0;
 }
+
